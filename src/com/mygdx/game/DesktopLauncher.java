9b6a233f795b147ed5e43488f570f9d0d13e3815
arctic1106 <arcticsoft@outlook.com>
2023-12-07 04:28:38 +0100

    cleanup

--- src/com/mygdx/game/DesktopLauncher.java
+++ src/com/mygdx/game/DesktopLauncher.java
@@ -1,8 +1,7 @@
-package com.mygdx.game.desktop;
+package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
